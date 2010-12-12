9c8c85f56c9f78ea8035af09af22fb588d3e5312
Matthew J Morrison <mattj.morrison@gmail.com>
2010-12-12 17:15:59 -0600

    removed specific ip and user/pass info

--- fabfiles/deploy.py
+++ fabfiles/deploy.py
@@ -1,10 +1,15 @@
 from fabric.api import env, run, cd
 
-env.hosts = ["184.106.218.153"]
-env.user = "root"
-env.password = "fabricsamplesR7gKg1Q1"
+env.hosts = ["ip"]
+env.user = "user"
+env.password = "password"
 
 def deploy():
+    try: #archive old one if it's already there
+        run("mv something old_something")
+    except:
+        pass
+
     run('mkdir something')
     print run('ls -la')
 
