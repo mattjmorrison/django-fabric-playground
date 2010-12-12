fbf00c6e4dc31f7c8c0a4c89388e1d463979ce2e
Matthew J Morrison <mattj.morrison@gmail.com>
2010-12-12 17:07:27 -0600

    moved some stuff around

--- fabfiles/deploy.py
+++ fabfiles/deploy.py
@@ -1,12 +1,8 @@
 from fabric.api import env, run, cd
 
-ip = "184.106.218.153"
-user = "root"
-password = "fabricsamplesR7gKg1Q1"
-
-env.hosts = [ip]
-env.user = user
-env.password = password
+env.hosts = ["184.106.218.153"]
+env.user = "root"
+env.password = "fabricsamplesR7gKg1Q1"
 
 def deploy():
     run('mkdir something')
